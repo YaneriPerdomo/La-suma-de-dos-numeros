60f822714eef93342a2d8a479d19c3876221464a
YaneriPerdomo <92409304+YaneriPerdomo@users.noreply.github.com>
2021-10-12 20:26:35 -0400

    Add files via upload

---
+++ la suma.cpp
@@ -0,0 +1,18 @@
+/* programa para calcular la suma de dos numeros dado*/
+//ingresamos librerias recomendada
+#include<iostream>
+using namespace std;
+int main(){
+    //valores de tipo entero
+    int a,b,suma
+    cout<<"ingresar primer valor: " ;cin>>a;
+    cout<<"ingresar segundo valor: " ;cin>>b;
+    //proceso de calulo de la suma
+    suma=a+b;
+    //salida del proceso
+    count<<"la suma de dichos dos numeros ingresados es "<<suma<<endl;
+    //compilamos"
+
+
+    return 0;
+}
